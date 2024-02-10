d91f19b6dee519da970f2d05780496681cdfd654
Jjuarezp7 <109290437+Jjuarezp7@users.noreply.github.com>
2024-02-09 18:01:27 -0600

    prototipo mantenimiento boleta

--- Equipo2/Codigo/Modulos/Migracion_G2/CapaVista_MG2/Man_Boleta.cs
+++ Equipo2/Codigo/Modulos/Migracion_G2/CapaVista_MG2/Man_Boleta.cs
@@ -16,5 +16,20 @@ namespace CapaVista_MG2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
