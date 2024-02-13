92778bbcb65ba1857530cb1589733bb4e51c7712
coradoa <90011754+coradoa@users.noreply.github.com>
2024-02-13 16:06:46 -0600

    ultimo commit

--- Equipo1/Codigo/Modulos/Prototipo/Ejecucion/Ejecucion/Program.cs
+++ Equipo1/Codigo/Modulos/Prototipo/Ejecucion/Ejecucion/Program.cs
@@ -13,7 +13,7 @@ namespace Ejecucion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Vista_PrototipoMenu.Calendario_citas());
+            Application.Run(new Vista_PrototipoMenu.HorariosDispo());
             
         }   
     }
