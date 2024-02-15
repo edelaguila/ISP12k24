03ecb8c315b812b91b138965234c8c91537ac88f
David Carrillo <carrillod421@gmail.com>
2024-02-14 18:24:46 -0600

    cambios

---
+++ Equipo1/Codigo/Modulos/Prototipo/Prototipo/Capa_controlador/Controlador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo_PrototipoMenu;
+
+namespace Controlador_PrototipoMenu
+{
+    public class Controlador
+    {
+        public Sentencias sn;
+
+        public Controlador()
+        {
+            sn = new Sentencias();
+        }
+        
+    
+    }
+}
