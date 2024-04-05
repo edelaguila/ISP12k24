4f27a7a0e7d5990bac0ef527a947dd537f29de0f
Luis Franco <91704866+Frls03@users.noreply.github.com>
2024-04-05 17:08:30 -0600

    agregando codigo, transferencias y tarjetas

--- Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/CodigoDeSeguridad.Designer.cs
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/CodigoDeSeguridad.Designer.cs
@@ -47,7 +47,7 @@ namespace CapaVista_SisB
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(139, 134);
+            this.label2.Location = new System.Drawing.Point(147, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(213, 36);
             this.label2.TabIndex = 1;
