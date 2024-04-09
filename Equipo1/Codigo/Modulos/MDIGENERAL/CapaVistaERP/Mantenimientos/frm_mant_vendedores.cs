4e274dfcbf49308ea4134afb7de2077c24440357
David Carrillo <carrillod421@gmail.com>
2024-04-09 17:24:49 -0600

    Agregando Guardar en bitacora lo realizado en Mantenimientos

--- Equipo1/Codigo/Modulos/MDIGENERAL/CapaVistaERP/Mantenimientos/frm_mant_vendedores.cs
+++ Equipo1/Codigo/Modulos/MDIGENERAL/CapaVistaERP/Mantenimientos/frm_mant_vendedores.cs
@@ -15,6 +15,8 @@ namespace CapaVistaERP.Mantenimientos
         public frm_mant_vendedores()
         {
             InitializeComponent();
+            this.navegador1.config("tbl_vendedor", this, "");
+            this.navegador1._initSeguridad();
         }
     }
 }
