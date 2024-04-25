2f567c878d544d76d8bff64736f86e62de24c7f1
carlos enrique <cguzmanc10@miumg.edu.gt>
2024-04-24 21:52:03 -0600

    60% del proceso movimiento de bancos realizado se ha implementado una bitacora para dichos movimientos

---
+++ Equipo1/Codigo/Modulos/MDIGENERAL/CapaVistaERP/Procesos/frmBitacoraMovimientoDeBancos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControladorERP;
+namespace CapaVistaERP.Procesos
+{
+    public partial class frmBitacoraMovimientoDeBancos : Form
+    {
+        Controlador cn = new Controlador();
+        public frmBitacoraMovimientoDeBancos()
+        {
+            InitializeComponent();
+        }
+        string emp = "tbl_movimientodebancos";
+
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenartablabitacoraMB(emp);
+            dtTabla.DataSource = dt;
+
+        }
+
+        public void BuscarPorFecha()
+        {
+            DateTime fechaSeleccionada = dateTimePicker1.Value;
+            string strfiltro = fechaSeleccionada.ToString("yyyy-MM-dd");
+            DataTable dt = cn.BuscarMB(strfiltro);
+            dtTabla.DataSource = dt;
+        }
+
+        private async void btn_regresar_Click(object sender, EventArgs e)
+        {
+            await Task.Delay(500);
+            this.Close();
+        }
+
+        private void btn_actualizar_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+
+        private void btn_Buscar_Click(object sender, EventArgs e)
+        {
+            BuscarPorFecha();
+        }
+    }
+}
