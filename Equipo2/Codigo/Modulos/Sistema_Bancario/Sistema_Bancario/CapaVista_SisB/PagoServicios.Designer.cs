ac7883d93b13784bf6e694024ad5402a5d8d8d4e
JustinPennant <jramosp14@miumg.edu.gt>
2024-04-25 17:20:24 -0600

    Actualización de Form de Pago de Servicios

--- Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/PagoServicios.Designer.cs
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/PagoServicios.Designer.cs
@@ -30,7 +30,6 @@ namespace CapaVista_SisB
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -38,43 +37,37 @@ namespace CapaVista_SisB
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.navegador1 = new Navegador2._0.Navegador();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 409);
+            this.dataGridView1.Location = new System.Drawing.Point(22, 171);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(863, 175);
+            this.dataGridView1.Size = new System.Drawing.Size(614, 175);
             this.dataGridView1.TabIndex = 28;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(45, 380);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(56, 17);
-            this.label8.TabIndex = 30;
-            this.label8.Text = "Detalle:";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(291, 11);
+            this.label9.Location = new System.Drawing.Point(105, 10);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(428, 55);
             this.label9.TabIndex = 31;
@@ -82,7 +75,7 @@ namespace CapaVista_SisB
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(561, 218);
+            this.textBox7.Location = new System.Drawing.Point(365, 363);
             this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(271, 22);
@@ -92,24 +85,25 @@ namespace CapaVista_SisB
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(463, 224);
+            this.label7.Location = new System.Drawing.Point(250, 368);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(91, 17);
+            this.label7.Size = new System.Drawing.Size(98, 17);
             this.label7.TabIndex = 38;
-            this.label7.Text = "Saldo Actual:";
+            this.label7.Text = "Total a Pagar:";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(152, 188);
+            this.textBox6.Location = new System.Drawing.Point(128, 90);
             this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(283, 22);
+            this.textBox6.Size = new System.Drawing.Size(26, 22);
             this.textBox6.TabIndex = 37;
             this.textBox6.Tag = "Usuario";
+            this.textBox6.Visible = false;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(152, 215);
+            this.textBox5.Location = new System.Drawing.Point(157, 123);
             this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(283, 22);
@@ -119,130 +113,155 @@ namespace CapaVista_SisB
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(17, 218);
+            this.label6.Location = new System.Drawing.Point(19, 126);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(129, 17);
             this.label6.TabIndex = 35;
-            this.label6.Text = "Código de Usuario:";
+            this.label6.Text = "Número de Cliente:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 191);
+            this.label5.Location = new System.Drawing.Point(19, 90);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.Size = new System.Drawing.Size(62, 17);
             this.label5.TabIndex = 34;
-            this.label5.Text = "Usuario:";
+            this.label5.Text = "Servicio:";
             // 
-            // textBox1
+            // button1
             // 
-            this.textBox1.Location = new System.Drawing.Point(152, 271);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(283, 22);
-            this.textBox1.TabIndex = 44;
-            this.textBox1.Tag = "Codigo_servicio";
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(465, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 52);
+            this.button1.TabIndex = 49;
+            this.button1.Text = "BUSCAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(465, 497);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 52);
+            this.button2.TabIndex = 50;
+            this.button2.Text = "PAGAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 271);
+            this.label1.Location = new System.Drawing.Point(19, 405);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
-            this.label1.TabIndex = 40;
-            this.label1.Text = "Código:";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Número de Cuenta:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(80, 298);
+            this.label2.Location = new System.Drawing.Point(19, 435);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 17);
-            this.label2.TabIndex = 41;
-            this.label2.Text = "Servicio:";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Nombre de Usuario:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 405);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(467, 22);
+            this.textBox1.TabIndex = 53;
+            this.textBox1.Tag = "Saldo_actual";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 435);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(467, 22);
+            this.textBox2.TabIndex = 54;
+            this.textBox2.Tag = "Saldo_actual";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(157, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(283, 24);
+            this.comboBox1.TabIndex = 55;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(503, 298);
+            this.label3.Location = new System.Drawing.Point(19, 465);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 17);
-            this.label3.TabIndex = 42;
-            this.label3.Text = "Monto:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Tipo de Cuenta:";
             // 
-            // label4
+            // comboBox2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(503, 332);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 17);
-            this.label4.TabIndex = 43;
-            this.label4.Text = "Fecha:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(152, 300);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(283, 22);
-            this.textBox2.TabIndex = 45;
-            this.textBox2.Tag = "Servicio";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(169, 465);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(467, 24);
+            this.comboBox2.TabIndex = 57;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(561, 289);
+            this.textBox3.Location = new System.Drawing.Point(134, 467);
             this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(283, 22);
-            this.textBox3.TabIndex = 46;
-            this.textBox3.Tag = "Monto";
+            this.textBox3.Size = new System.Drawing.Size(29, 22);
+            this.textBox3.TabIndex = 58;
+            this.textBox3.Tag = "Usuario";
+            this.textBox3.Visible = false;
             // 
-            // textBox4
+            // panel1
             // 
-            this.textBox4.Location = new System.Drawing.Point(561, 329);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(283, 22);
-            this.textBox4.TabIndex = 47;
-            this.textBox4.Tag = "Fecha";
-            // 
-            // navegador1
-            // 
-            this.navegador1.Location = new System.Drawing.Point(20, 70);
-            this.navegador1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(1001, 96);
-            this.navegador1.TabIndex = 48;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(13, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 75);
+            this.panel1.TabIndex = 59;
             // 
             // PagoServicios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1053, 594);
-            this.Controls.Add(this.navegador1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(659, 561);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PagoServicios";
             this.Text = "Realizar pago de Servicios";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -250,7 +269,6 @@ namespace CapaVista_SisB
 
         #endregion
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
@@ -258,14 +276,16 @@ namespace CapaVista_SisB
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private Navegador2._0.Navegador navegador1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
