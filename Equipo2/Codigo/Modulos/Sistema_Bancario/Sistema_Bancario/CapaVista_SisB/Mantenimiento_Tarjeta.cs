8f99553d31e7542de659c271e2f3e8c30be1f884
D4nnny <dperezf5@miumg.edu.gt>
2024-05-07 17:25:18 -0600

    Integración de Mantenimientos al MDI

---
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/Mantenimiento_Tarjeta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Seguridad_Controlador;
+
+namespace CapaVista_SisB
+{
+    public partial class Mantenimiento_Tarjeta : Form
+    {
+        //Codigo de configuracion al navegador
+        public Mantenimiento_Tarjeta()
+        {
+            InitializeComponent();
+            navegador1.config("tbl_tarjeta", this, "8003");
+        }
+    }
+}
---
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/Mantenimiento_Tipo_Tarjeta.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CapaVista_SisB
+{
+    partial class Mantenimiento_Tipo_Tarjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador2 = new Navegador2._0.Navegador();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // navegador2
+            // 
+            this.navegador2.Location = new System.Drawing.Point(24, 13);
+            this.navegador2.Margin = new System.Windows.Forms.Padding(4);
+            this.navegador2.Name = "navegador2";
+            this.navegador2.Size = new System.Drawing.Size(751, 78);
+            this.navegador2.TabIndex = 54;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Id tipo tarjeta:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 55;
+            this.textBox1.Tag = "ttar_id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "Nombre tipo tarjeta:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(524, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 58;
+            this.textBox2.Tag = "ttar_nombre";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(751, 81);
+            this.dataGridView1.TabIndex = 59;
+            this.dataGridView1.Tag = "tbl_tipo_tarjeta";
+            // 
+            // Mantenimiento_Tipo_Tarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 278);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.navegador2);
+            this.Name = "Mantenimiento_Tipo_Tarjeta";
+            this.Text = "Mantenimiento_Tipo_Tarjeta";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Navegador2._0.Navegador navegador2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
