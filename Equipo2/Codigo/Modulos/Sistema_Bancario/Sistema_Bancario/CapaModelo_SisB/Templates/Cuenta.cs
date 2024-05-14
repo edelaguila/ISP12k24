bf7771ecf4f1a9161c8f4d13e4a02ba6f4f5c144
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-05-14 00:14:00 -0600

    account class

--- Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaModelo_SisB/Templates/Cuenta.cs
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaModelo_SisB/Templates/Cuenta.cs
@@ -11,12 +11,15 @@ namespace CapaModelo_SisB.Templates
         public int id;
         public int idCliente;
         public double saldo;
+        public string nombre;
         public int idTipo;
         public int idMoneda;
         public string numero;
         public int idUsuario;
+        public List<Cuenta> friend_accounts = new List<Cuenta>();
 
-        public Cuenta(int id, int idCliente, double saldo, int idTipo, int idMoneda, string numero, int idUsuario)
+
+        public Cuenta(int id, int idCliente, double saldo, int idTipo, int idMoneda, string numero, int idUsuario, string nombre)
         {
             this.id = id;
             this.idCliente = idCliente;
@@ -25,6 +28,30 @@ namespace CapaModelo_SisB.Templates
             this.idMoneda = idMoneda;
             this.numero = numero;
             this.idUsuario = idUsuario;
+            this.nombre = nombre;
+        }
+
+        public Cuenta(int id, string name, string no_account, double balance)
+        {
+            this.saldo = balance;
+            this.id = id;
+            this.nombre = name;
+            this.numero = no_account;
+        }
+
+        public void updateTemplateAttributes(int idCliente, double saldo, int idTipo, int idMoneda, string numero, int idUsuario, string nombre)
+        {
+            this.idCliente = idCliente;
+            this.saldo = saldo;
+            this.idTipo = idTipo;
+            this.idMoneda = idMoneda;
+            this.numero = numero;
+            this.idUsuario = idUsuario;
+            this.nombre = nombre;
+        }
+        public void updateBalance(double saldo)
+        {
+            this.saldo = saldo;
         }
     }
 }
