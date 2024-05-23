3ed8541cc37eefcb2e43bfd5eb811ed68932ec44
Otto Ventura <139597154+OTEKE10@users.noreply.github.com>
2024-05-23 08:30:23 -0600

    Agregando API ERP
    Se agrega la API

---
+++ Equipo1/API/ApiERP/ApiERP/Models/Connection.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Odbc;
+namespace ApiERP.Models
+{
+    public class Connection
+    {
+        public OdbcConnection myconn;
+        public Connection()
+        {
+            this.myconn = new OdbcConnection("Dsn=HotelSConexion");
+        }
+
+
+        public OdbcConnection connection()
+        {
+            try
+            {
+                this.myconn = new OdbcConnection("Dsn=HotelSConexion");
+                myconn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return myconn;
+        }
+
+
+        public void disconnect(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+    }
+}
---
+++ Equipo1/API/ApiERP/ApiERP/Models/ProveedorSentence.cs
@@ -0,0 +1,70 @@
+﻿using ApiERP.Models.Templates;
+using System.Data.Odbc;
+using ApiERP.Models;
+
+namespace ApiERP.Models
+{
+    public class ProveedorSentence
+    {
+        public Connection conn;
+
+        public ProveedorSentence()
+        {
+            this.conn = new Connection();
+        }
+
+
+        public List<Proveedor> getAllProvs()
+        {
+            List<Proveedor> provs = new List<Proveedor>();
+            string sql = "select * from tbl_proveedor";
+            OdbcCommand cmd = new OdbcCommand(sql, this.conn.connection());
+            OdbcDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                provs.Add(new Proveedor(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5)));
+            }
+            return provs;
+        }
+
+        public Proveedor getProvByNit(string nit)
+        {
+            string sql = "select * from tbl_proveedor where nit_prov='" + nit + "'";
+            OdbcCommand cmd = new OdbcCommand(sql, this.conn.connection());
+            OdbcDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return new Proveedor(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5));
+            }
+            return new Proveedor(0, "", "", "", "", "");
+        }
+
+        public InvoiceP getInvoiceP(string nit)
+        {
+            string sql = "SELECT * FROM tbl_facturaxpagar where nitprov_facxpag='" + nit + "';";
+            OdbcCommand cmd = new OdbcCommand(sql, this.conn.connection());
+            OdbcDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return new InvoiceP(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetDate(3), reader.GetDate(4), reader.GetDouble(5), reader.GetDouble(6), reader.GetDouble(7), reader.GetInt32(8), reader.GetString(9), reader.GetInt32(10));
+            }
+            return null;
+        }
+
+        public List<InvoiceP> getAllInvoices()
+        {
+            List<InvoiceP> invoices = new List<InvoiceP>();
+            string sql = "select * from tbl_facturaxpagar";
+            OdbcCommand cmd = new OdbcCommand(sql, this.conn.connection());
+            OdbcDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                InvoiceP inv = new InvoiceP(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetDate(3), reader.GetDate(4), reader.GetDouble(5), reader.GetDouble(6), reader.GetDouble(7), reader.GetInt32(8), reader.GetString(9), reader.GetInt32(10));
+                invoices.Add(inv);
+            }
+            return invoices;
+        }
+
+
+    }
+}
