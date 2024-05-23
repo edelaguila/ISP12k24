0f6946d2a280b2d7226479f4e4eb55334d650f1c
DiegoMaroquin <antomarro2000@gmail.com>
2024-05-22 18:36:55 -0600

    se agregra 92% tipodecambioApi

---
+++ Equipo1/Codigo/Modulos/MDIGENERAL/CapaVistaERP/Procesos/TipodecambioApi.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace CapaVistaERP.Procesos
+{
+    partial class TipodecambioApi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TipodecambioApi));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbMonedas = new System.Windows.Forms.ComboBox();
+            this.PAIS = new System.Windows.Forms.Label();
+            this.dgvTipoCambio = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_nueva = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_ayudas = new System.Windows.Forms.Button();
+            this.btn_reporte = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCambio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(3)))), ((int)(((byte)(38)))));
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btn_nueva);
+            this.panel1.Controls.Add(this.btn_salir);
+            this.panel1.Controls.Add(this.btn_ayudas);
+            this.panel1.Controls.Add(this.btn_reporte);
+            this.panel1.Location = new System.Drawing.Point(135, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 101);
+            this.panel1.TabIndex = 205;
+            // 
+            // cbMonedas
+            // 
+            this.cbMonedas.FormattingEnabled = true;
+            this.cbMonedas.Location = new System.Drawing.Point(46, 231);
+            this.cbMonedas.Name = "cbMonedas";
+            this.cbMonedas.Size = new System.Drawing.Size(242, 24);
+            this.cbMonedas.TabIndex = 204;
+            // 
+            // PAIS
+            // 
+            this.PAIS.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PAIS.AutoSize = true;
+            this.PAIS.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PAIS.Location = new System.Drawing.Point(62, 211);
+            this.PAIS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PAIS.Name = "PAIS";
+            this.PAIS.Size = new System.Drawing.Size(71, 17);
+            this.PAIS.TabIndex = 203;
+            this.PAIS.Text = "MONEDA";
+            // 
+            // dgvTipoCambio
+            // 
+            this.dgvTipoCambio.AllowUserToAddRows = false;
+            this.dgvTipoCambio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvTipoCambio.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvTipoCambio.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTipoCambio.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvTipoCambio.ColumnHeadersHeight = 40;
+            this.dgvTipoCambio.Location = new System.Drawing.Point(46, 360);
+            this.dgvTipoCambio.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvTipoCambio.Name = "dgvTipoCambio";
+            this.dgvTipoCambio.RowHeadersWidth = 40;
+            this.dgvTipoCambio.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvTipoCambio.RowTemplate.Height = 25;
+            this.dgvTipoCambio.Size = new System.Drawing.Size(648, 150);
+            this.dgvTipoCambio.TabIndex = 198;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(541, 207);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 17);
+            this.label6.TabIndex = 200;
+            this.label6.Text = "Año";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(435, 207);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 17);
+            this.label5.TabIndex = 201;
+            this.label5.Text = "Mes";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(333, 207);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 202;
+            this.label4.Text = "Día";
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtAnio.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnio.Location = new System.Drawing.Point(545, 229);
+            this.txtAnio.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAnio.MaxLength = 4;
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(132, 26);
+            this.txtAnio.TabIndex = 197;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtMes.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes.Location = new System.Drawing.Point(439, 229);
+            this.txtMes.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMes.MaxLength = 2;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(79, 26);
+            this.txtMes.TabIndex = 196;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtDia.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDia.Location = new System.Drawing.Point(337, 229);
+            this.txtDia.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDia.MaxLength = 2;
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(79, 26);
+            this.txtDia.TabIndex = 195;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMensaje.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblMensaje.Location = new System.Drawing.Point(-3, 338);
+            this.lblMensaje.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(748, 18);
+            this.lblMensaje.TabIndex = 199;
+            this.lblMensaje.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(113)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Snow;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(93, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 71);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Guardar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_nueva
+            // 
+            this.btn_nueva.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(113)))));
+            this.btn_nueva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_nueva.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nueva.ForeColor = System.Drawing.Color.Snow;
+            this.btn_nueva.Image = ((System.Drawing.Image)(resources.GetObject("btn_nueva.Image")));
+            this.btn_nueva.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_nueva.Location = new System.Drawing.Point(3, 13);
+            this.btn_nueva.Name = "btn_nueva";
+            this.btn_nueva.Size = new System.Drawing.Size(84, 71);
+            this.btn_nueva.TabIndex = 17;
+            this.btn_nueva.Text = "Consultar";
+            this.btn_nueva.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_nueva.UseVisualStyleBackColor = false;
+            this.btn_nueva.Click += new System.EventHandler(this.btn_nueva_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(113)))));
+            this.btn_salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.ForeColor = System.Drawing.Color.Snow;
+            this.btn_salir.Image = ((System.Drawing.Image)(resources.GetObject("btn_salir.Image")));
+            this.btn_salir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_salir.Location = new System.Drawing.Point(363, 13);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(84, 71);
+            this.btn_salir.TabIndex = 6;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_salir.UseVisualStyleBackColor = false;
+            // 
+            // btn_ayudas
+            // 
+            this.btn_ayudas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(113)))));
+            this.btn_ayudas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ayudas.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ayudas.ForeColor = System.Drawing.Color.Snow;
+            this.btn_ayudas.Image = ((System.Drawing.Image)(resources.GetObject("btn_ayudas.Image")));
+            this.btn_ayudas.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_ayudas.Location = new System.Drawing.Point(273, 13);
+            this.btn_ayudas.Name = "btn_ayudas";
+            this.btn_ayudas.Size = new System.Drawing.Size(84, 71);
+            this.btn_ayudas.TabIndex = 5;
+            this.btn_ayudas.Text = "Ayuda";
+            this.btn_ayudas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_ayudas.UseVisualStyleBackColor = false;
+            // 
+            // btn_reporte
+            // 
+            this.btn_reporte.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(113)))));
+            this.btn_reporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_reporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reporte.ForeColor = System.Drawing.Color.Snow;
+            this.btn_reporte.Image = ((System.Drawing.Image)(resources.GetObject("btn_reporte.Image")));
+            this.btn_reporte.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_reporte.Location = new System.Drawing.Point(183, 13);
+            this.btn_reporte.Name = "btn_reporte";
+            this.btn_reporte.Size = new System.Drawing.Size(84, 71);
+            this.btn_reporte.TabIndex = 4;
+            this.btn_reporte.Text = "Reporte";
+            this.btn_reporte.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_reporte.UseVisualStyleBackColor = false;
+            // 
+            // TipodecambioApi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 560);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cbMonedas);
+            this.Controls.Add(this.PAIS);
+            this.Controls.Add(this.dgvTipoCambio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "TipodecambioApi";
+            this.Text = "TipodecambioApi";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCambio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_nueva;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_ayudas;
+        private System.Windows.Forms.Button btn_reporte;
+        private System.Windows.Forms.ComboBox cbMonedas;
+        private System.Windows.Forms.Label PAIS;
+        private System.Windows.Forms.DataGridView dgvTipoCambio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label lblMensaje;
+    }
+}
