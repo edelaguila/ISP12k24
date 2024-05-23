60cb33b97519b53e7540a25336b8e0bd0b49b7f8
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-05-22 22:28:43 -0600

    transaction document fixed

--- Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/frmDeposito.cs
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaVista_SisB/frmDeposito.cs
@@ -12,10 +12,6 @@ using Seguridad_Controlador;
 using CapaModelo_SisB.Libraries;
 using CapaModelo_SisB.Templates;
 using System.IO;
-using iTextSharp;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-using System.Diagnostics;
 
 namespace CapaVista_SisB
 {
@@ -28,47 +24,6 @@ namespace CapaVista_SisB
             InitializeComponent();
         }
 
-        public void Comprobante()
-        {
-            string folderPath = @"D:\9NO_SEMESTRE\INGENIERIA DE SOFTWARE I\ISP12k24\Equipo2\Codigo\Modulos\Sistema_Bancario\Sistema_Bancario\CapaVista_SisB\Comprobantes";
-            string fileName = "Comprobante.pdf";
-            string filePath = Path.Combine(folderPath, fileName);
-
-            // Asegúrate de que el directorio existe
-            Directory.CreateDirectory(folderPath);
-
-            // Crear el documento PDF
-            Document doc = new Document();
-
-            // Crear el FileStream con la ruta específica
-            PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
-
-            // Abrir el documento para agregar contenido
-            doc.Open();
-
-            // Agregar contenido al documento
-            Paragraph paragraph = new Paragraph("Comprobante de Pago")
-            {
-                Alignment = Element.ALIGN_CENTER
-            };
-            // Agregar contenido al documento
-            doc.Add(paragraph);
-            doc.Add(new Paragraph("Deposito a Cuenta: "));
-            doc.Add(new Paragraph("\n"));
-            doc.Add(new Paragraph(txt_cuenta.Text));
-            doc.Add(new Paragraph("\n"));
-            doc.Add(new Paragraph("Monto: "));
-            doc.Add(new Paragraph("\n"));
-            doc.Add(new Paragraph("Q " + monto.ToString()));
-          
-
-            // Cerrar el documento
-            doc.Close();
-
-            Console.WriteLine("PDF creado en: " + filePath);
-
-            OpenPDF(filePath);
-        }
 
 
 
@@ -108,29 +63,9 @@ namespace CapaVista_SisB
 
         private void button2_Click_1(object sender, EventArgs e)
         {
-            Comprobante();
-
+            DocumentController.makeTransactionDocument(txt_cuenta.Text, textBox1.Text);
         }
 
-        private void OpenPDF(string filePath)
-        {
-            try
-            {
-                // Verificar si el archivo existe antes de intentar abrirlo
-                if (File.Exists(filePath))
-                {
-                    Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
-                }
-                else
-                {
-                    MessageBox.Show("El archivo no se encontró.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ocurrió un error al intentar abrir el PDF: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+
     }
 }
-   
