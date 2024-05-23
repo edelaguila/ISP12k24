ba3d2a7151fa97120f4f1dc7270e6a35a6ff1dd9
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-05-22 20:40:43 -0600

    gestionando tarjetas

---
+++ Equipo2/Codigo/Modulos/Sistema_Bancario/Sistema_Bancario/CapaModelo_SisB/Sentences/ClienteSentence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace CapaModelo_SisB.Sentences
+{
+    public class ClienteSentence
+    {
+        static Conexion conn = new Conexion();
+
+        public static int getClientIdByDPI(string dpi)
+        {
+            string sql = "select cli_id from tbl_cliente where cli_dpi='" + dpi + "'";
+            OdbcCommand cmd = new OdbcCommand(sql, conn.connection());
+            OdbcDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (reader.IsDBNull(0)) return -1;
+                return reader.GetInt32(0);
+            }
+            return -1;
+        }
+    }
+}
