b5e43a2b18513be1b96b2b5c8e1ffa496304c4c0
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-05-24 09:17:22 -0600

    Add files via upload

---
+++ Equipo2/WebApi1/WebApi1/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi1.Models;
+using CapaModelo_SisB;
+
+
+namespace WebApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountSentence accountSentence;
+        public InvoiceSentence invSentence;
+        public AccountController()
+        {
+            this.accountSentence = new AccountSentence();
+            this.invSentence = new InvoiceSentence();
+        }
+
+        // GET: api/<AccountController>
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return this.accountSentence.getAccounts().ToList();
+        }
+
+        // GET api/<AccountController>/4543554
+        [HttpGet("{id}")]
+        public Account Get(string id)
+        {
+            return this.accountSentence.getAccountByNit(id);
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut]
+        public Response<Invoice> Put([FromBody] Invoice invoice)
+        {
+            if (this.accountSentence.checkPaymentViability(invoice.clientNit, invoice.amount))
+            {
+                this.accountSentence.makeServicePayment(invoice.clientNit, invoice.amount);
+                invoice.status = true;
+                this.invSentence.changeInvoiceStatus(invoice.id);
+                return new Response<Invoice>(300, "Pago realizado correctamente", invoice);
+            }
+            return new Response<Invoice>(402, "Fondos insuficientes para realizar el pago", invoice);
+
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Equipo2/WebApi1/WebApi1/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi1.Models
+{
+    public class Response<T>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public Response(int code, string message, T data)
+        {
+            Code = code;
+            Message = message;
+            Data = data;
+        }
+    }
+}
